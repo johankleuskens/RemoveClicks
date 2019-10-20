1b9e7657f350b1c8d43a5bb5aa2fa9c8666295b4
johan <johan@melderse.nl>
2019-10-20 19:58:59 +0200

    Slow processing working

--- remove_clicks.py
+++ remove_clicks.py
@@ -7,7 +7,6 @@ Created on Oct 19, 2019
 import scipy.io.wavfile as wav
 import os
 import numpy as np
-from numpy import nditer
 
 
 def load_wav_file(wav_file):
@@ -21,28 +20,67 @@ def write_wav_file(wav_left, wav_right, name):
     wav.write(name+'piet.wav', 48000, sig)
     
 def ProcessChannel(channel):
-    #for sample in nditer(channel):
-        #print(sample)
-    
-    return channel
+    min_level = -0.65
+    prev_progress = int(0);
+    processed_channel = np.copy(channel)
+    for x in range(processed_channel.size):
+        if processed_channel[x] < min_level:
+            count = 0;
+            while processed_channel[x+count] < min_level:
+                count = count +1
+            if count > 8:
+                processed_channel[x:x+count] = 0
+        # Print progress
+        progress = 100 * x/channel.size
+        if int(progress) != prev_progress:
+            prev_progress = int(progress)
+            print('Progress ', prev_progress)
+            
+    return processed_channel
+
+def ProcessChannelFast(channel):
+    min_level = -0.65
+    prev_progress = int(0);
+    # Make a copy of channel data because original data is read only
+    processed_channel = np.copy(channel)
+    # Create an array of minimum values
+    min_channel = np.ones(channel.size) * min_level
+    # Create an array with indexes where processed_Channel is  < min_level
+    hit_channel = processed_channel < min_channel
+    skip_index = 0
+    for x in range(hit_channel):
+        # check if this index should be skipped as it has been processed already
+        if x < skip_index:
+            continue
+        #Check the hit_channel array for consecutive index values
+        count = 0
+        while hit_channel[x + count] + 1 == hit_channel[x + count + 1]:
+            count = count +1
+        if count > 8:
+            processed_channel[x:x+count] = 0
+            skip_index = x + count
+        
+        # Print progress
+        progress = 100 * x/channel.size
+        if int(progress) != prev_progress:
+            prev_progress = int(progress)
+            print('Progress ', prev_progress)
+            
+    return processed_channel
 
 def ProcessAllWavFiles(path):
     # find all wave files in current path
-    for root, dirs, files in os.walk(path):
-        wav_files = [file for file in files if file.endswith('.wav')]
-        for wav_file in wav_files:
-            print("Loading ", wav_file)
-            sig_left, sig_right = load_wav_file(path + wav_file)
-            print("Processing ", wav_file)
-
-            sig_left_new = ProcessChannel(sig_left)
-            sig_right_new = ProcessChannel(sig_right)
-            # Write result back to new file
-            write_wav_file(sig_left_new, sig_right_new, path)
-            print("Ready ", wav_file)
-
-    
-    
+    files = os.listdir(path)
+    wav_files = [file for file in files if file.endswith('.wav')]
+    for wav_file in wav_files:
+        print("Loading ", wav_file)
+        sig_left, sig_right = load_wav_file(path + wav_file)
+        print("Processing ", wav_file)
+        sig_left_new = ProcessChannel(sig_left)
+        sig_right_new = ProcessChannel(sig_right)
+        # Write result back to new file
+        write_wav_file(sig_left_new, sig_right_new, path+'/ProcessedWav/')
+        print("Ready ", wav_file)
 
 #if __name__ == '__main__':
 ProcessAllWavFiles('/mnt/hgfs/DVOmzetten/')
